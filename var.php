ab6f03dadd195c610b50c8af6cee4f946442d3d1
vincent <vincent-du03@hotmail.fr>
2017-03-07 16:11:18 +0100

    exo

--- var.php
+++ var.php
@@ -2,10 +2,37 @@
 $nom= 'chambonnier';
 $prenom='vincent';
 $age=20;
-$kilometre= 125;
+$kilometre= 1;
 
 echo $nom;
 echo $prenom;
 echo $age;
 echo $kilometre;
+$kilometre=3;
+echo $kilometre;
+$kilometre=125;
+echo $kilometre;
+$int=50;
+echo $int;
+$float=5.5;
+echo $float;
+$bolean=true;
+echo $bolean;
+$string="bonjour";
+echo $bolean;
+$int2;
+$int2=30;
+echo $int2;
+$name= 'chambonnier';
+echo "bonjour,".$name.",comment ca va?";
+$nom2="chambonnier";
+$prenom2="vincent";
+$age3=20;
+echo "bonjour, ".$nom2." ".$prenom2.", tu as ".$age3." ans";
+$calcule1= 3+4;
+echo $calcule1;
+$calcule2= 5*20;
+echo $calcule2;
+$calcule3=45/5;
+echo $calcule3;
 ?>
