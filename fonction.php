7e5b6de9fd9fa1b2bc4c8b692dbdd2258e1e1088
vincent <vincent-du03@hotmail.fr>
2017-03-08 13:33:19 +0100

    exo 1 a 6

--- fonction.php
+++ fonction.php
@@ -1,8 +1,43 @@
 <?php
-// EXO 1 ///
+// EXO 1
 function truetrue(){
-	echo true
+        echo true."</br>";
 }
-// EXO 2 //
+truetrue();
+// EXO 2
+function chaine($string){
+        return $string."</br>";
+}
+echo chaine("bonjour");
 
-function chaine()
+// EXO 3
+    function double_chaine($string1, $string2){
+        return $string1 ." ". $string2 ."</br>";
+   }
+echo double_chaine('Phrase 1','Phrase 2');
+
+
+ // EXO 4
+    function nombres($n1, $n2){
+        if($n1 == $n2){
+            echo "Les deux nombres sont identiques.</br>";
+        }else if($n1 >= $n2){
+            echo "Le premier nombre est plus grand.</br>";
+        }else if($n1 <= $n2){
+            echo "Le premier nombre est plus petit.</br>";
+        }
+    }
+nombres(23, 30);
+
+// EXO 5 
+
+ function nombre_et_string($nombre, $phrase){
+        return $nombre ." ". $phrase ."</br>";
+    }
+echo nombre_et_string(22, "je veux manger");
+
+// EXO 6 //
+
+function npa($nom,$prenom,$age){
+	return "bonjour".$nom.$prenom."tu as ".$age."ans";
+}
